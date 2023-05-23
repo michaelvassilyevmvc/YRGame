5b100343b44d614a444a5e5fed8ea287c0ab2536
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-05-23 12:16:16 +0600

    Add project files.

---
+++ YRGame.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace YRGame.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ YRGame.Core/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YRGame.Core.Models
+{
+    public class Question
+    {
+        [Key]
+        public long Id { get; set; }
+        public long CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Answer { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
